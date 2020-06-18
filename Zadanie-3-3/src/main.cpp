66f5c2c6c09d9845ed01995fc238c6ae68b8adb3
Mikołaj Żochowski <mikolaj.zochowski@student.wat.edu.pl>
2020-06-18 09:34:39 +0200

    Wykonano zadanie 3.3.2 – tekst pływający

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,15 +1,34 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal  lcd(12, 11, 5, 4, 3, 2);
+char tablica[] = "Mikolaj";
+int tabLen = 0;
 
+void lcdClear(void);
 void setup() {
 lcd.begin(16, 2);
-lcd.print("Zochowski");
+lcd.print("Mikolaj Zochowski");
+delay(2000);
 }
 
 void loop() {
+for (int i = 0; i < (18 - tabLen); i++) {
+lcdClear();
+lcd.setCursor(i, 0);
+lcd.print(tablica);
+delay(1000);
+}
+for (int i = (17 - tabLen); i >= 0; i--) {
+lcdClear();
+lcd.setCursor(i, 1);
+lcd.print(tablica);
+delay(1000);
+}
+}
+void lcdClear(void) {
+lcd.setCursor(0, 0);
+lcd.print("................");
 lcd.setCursor(0, 1);
-lcd.print(millis() / 1000);
-delay(500);
+lcd.print("................");
 }
